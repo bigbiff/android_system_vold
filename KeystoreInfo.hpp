0156b7557edba1fd7146c744f3721e805c65bc08
bigbiff <bigbiff@teamw.in>
2022-03-12 16:43:12 -0500

    metadata decrypt: set property for twrp to know metadata device
    Change-Id: Icf3618159fc27bb2b8bae642f41c5e73c101e759

---
+++ KeystoreInfo.hpp
@@ -0,0 +1,32 @@
+/*
+		TWRP is free software: you can redistribute it and/or modify
+		it under the terms of the GNU General Public License as published by
+		the Free Software Foundation, either version 3 of the License, or
+		(at your option) any later version.
+
+		TWRP is distributed in the hope that it will be useful,
+		but WITHOUT ANY WARRANTY; without even the implied warranty of
+		MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+		GNU General Public License for more details.
+
+		You should have received a copy of the GNU General Public License
+		along with TWRP.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <string>
+#include <cutils/multiuser.h>
+
+#ifndef __KEYSTOREINFO_H
+#define __KEYSTOREINFO_H
+
+#define SYNTHETIC_PASSWORD_KEY_PREFIX "synthetic_password_"
+class KeystoreInfo {
+public:
+std::string getHandle(const userid_t user_id);
+std::string getAlias(std::string handle);
+
+private:
+std::string uint2hex(int64_t num);
+};
+
+#endif //__KEYSTOREINFO_H
